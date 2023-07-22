ab84797f95b8e6a87900fef521c68d34fa3e2237
Trevor Sherrard <sherrardtr@gmail.com>
2023-07-21 20:43:32 -0400

    removes debugging code

--- AttitudeEstimation/AttitudeEstimation.ino
+++ AttitudeEstimation/AttitudeEstimation.ino
@@ -1,21 +1,10 @@
 #include "Nicla_System.h"
 #include "Matrix/Matrix.hpp"
-#include <vector>
-
-std::vector<int> t(5);
 
 void setup() {
-  for(int i = 0; i < t.size(); i++)
-  {
-    t.at(i) = i;
-  }
-  Serial.begin(9600);
-  Serial.println("here");
+
 }
 
 void loop() {
-  for(int i = 0; i < t.size(); i++)
-  {
-    Serial.println(t[i]);
-  }
+
 }
