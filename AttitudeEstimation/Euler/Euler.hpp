483bd96ea06fb3adf2626afeb7015871197af825
Trevor Sherrard <sherrardtr@gmail.com>
2023-09-22 10:09:25 -0400

    finish initial Euler angle class implementation

--- AttitudeEstimation/Euler/Euler.hpp
+++ AttitudeEstimation/Euler/Euler.hpp
@@ -1,27 +1,33 @@
 #pragma once
 
+#include "../Angle/Angle.hpp"
+#include "../Quaternion/Quaternion.hpp"
+
 namespace structures {
 template <typename T> class Euler {
 public:
   /**
    * @brief construct and euler class with provided values.
-   * @param x rotation around x axis
-   * @param y rotation around y axis
-   * @param z rotation around z axis
+   * @param x Angle rotation around x axis
+   * @param y Angle rotation around y axis
+   * @param z Angle rotation around z axis
+   * @param angle_types the type of angles given in this constructor
    */
-  Euler(T x, T y, T z) {
-    this->_x = x;
-    this->_y = y;
-    this->_z = z;
+  Euler(T x, T y, T z, angle_type_t angle_types) {
+    this->_x = Angle(x, angle_types);
+    this->_y = Angle(y, angle_types);
+    this->_z = Angle(z, angle_types);
+    this->_angle_types = angle_types;
   }
 
   /**
-   * @brief constuct a zero euler
+   * @brief constuct a zero euler of angle type degrees
    */
   Euler() {
-    this->_x = 0;
-    this->_y = 0;
-    this->_z = 0;
+    this->_x = Angle((T)0, DEGREES);
+    this->_y = Angle((T)0, DEGREES);
+    this->_z = Angle((T)0, DEGREES);
+    this->_angle_types = DEGREES;
   }
 
   /**
@@ -31,6 +37,7 @@ public:
     this->_x = other.getX();
     this->_y = other.getY();
     this->_z = other.getZ();
+    this->_angle_types = other.getAngleType();
   }
 
   /**
@@ -41,6 +48,7 @@ public:
     this->_x = other.getX();
     this->_y = other.getY();
     this->_z = other.getZ();
+    this->_angle_types = other.getAngleType();
 
     return *this;
   }
@@ -49,41 +57,102 @@ public:
    * @brief gets the X component of the euler angle
    * @return the X component of the euler angle
    */
-  T getX() const { return this->_x; }
+  Angle<T> getX() const { return this->_x; }
 
   /**
    * @brief gets the Y component of the euler angle
    * @return the Y component of the euler angle
    */
-  T getY() const { return this->_y; }
+  Angle<T> getY() const { return this->_y; }
 
   /**
    * @brief gets the Z component of the euler angle
    * @return the Z component of the euler angle
    */
-  T getZ() const { return this->_z; }
+  Angle<T> getZ() const { return this->_z; }
 
   /**
    * @brief sets the rotation about the x axis.
    * @param x the new rotation about the x axis.
    */
-  void setX(T x) { this->_x = x; }
+  void setX(Angle<T> x) { this->_x = x; }
 
   /**
    * @brief sets the rotation about the y axis.
    * @param y the new rotation about the y axis.
    */
-  void setY(T y) { this->_y = y; }
+  void setY(Angle<T> y) { this->_y = y; }
 
   /**
    * @brief sets the rotation about the z axis.
    * @param z the new rotation about the z axis.
    */
-  void setZ(T z) { this->_z = z; }
+  void setZ(Angle<T> z) { this->_z = z; }
+
+  /**
+   * @brief get angle type of this class instance
+   * @return the angle type of this class instance
+   */
+  angle_type_t getAngleType() const { return this->_angle_types; }
+
+  /**
+   * @brief convert all angles to radians, if not already in radians
+   */
+  void toRadians() {
+    if (_angle_types == DEGREES) {
+      this->_angle_types = RADIANS;
+      this->_x.toRadians();
+      this->_y.toRadians();
+      this->_z.toRadians();
+    }
+  }
+
+  /**
+   * @brief convert all angles to degrees, if not already in degrees
+   */
+  void toDegrees() {
+    if (_angle_types == RADIANS) {
+      this->_angle_types = DEGREES;
+      this->_x.toDegrees();
+      this->_y.toDegrees();
+      this->_z.toDegrees();
+    }
+  }
+
+  /**
+   * @brief generate a new quaternion from this Euler class instance.
+   * @return
+   */
+  Quaternion<T> toQuaternion() {
+    // make sure we are in units of Radians
+    Euler<T> euler_to_convert = *this;
+
+    if (euler_to_convert.getAngleType() == DEGREES) {
+      euler_to_convert.toRadians();
+    }
+
+    // roll (x), pitch (y), yaw (z)
+    T roll = euler_to_convert.getX().getAngleValue();
+    T pitch = euler_to_convert.getY().getAngleValue();
+    T yaw = euler_to_convert.getZ().getAngleValue();
+
+    // compute Quaternion components
+    T w = cos(roll / 2) * cos(pitch / 2) * cos(yaw / 2) +
+          sin(roll / 2) * sin(pitch / 2) * sin(yaw / 2);
+    T x = sin(roll / 2) * cos(pitch / 2) * cos(yaw / 2) -
+          cos(roll / 2) * sin(pitch / 2) * sin(yaw / 2);
+    T y = cos(roll / 2) * sin(pitch / 2) * cos(yaw / 2) +
+          sin(roll / 2) * cos(pitch / 2) * sin(yaw / 2);
+    T z = cos(roll / 2) * cos(pitch / 2) * sin(yaw / 2) -
+          sin(roll / 2) * sin(pitch / 2) * cos(yaw / 2);
+
+    return Quaternion<T>(x, y, z, w);
+  }
 
 private:
-  T _x;
-  T _y;
-  T _z;
+  Angle<T> _x;
+  Angle<T> _y;
+  Angle<T> _z;
+  angle_type_t _angle_types;
 }; // end of Euler class
 } // namespace structures
--- AttitudeEstimation/Euler/test_euler.cpp
+++ AttitudeEstimation/Euler/test_euler.cpp
@@ -1,3 +1,4 @@
+#include "../Quaternion/Quaternion.hpp"
 #include "Euler.hpp"
 #include "gtest/gtest.h"
 using namespace structures;
@@ -5,46 +6,111 @@ using namespace structures;
 TEST(EulerClassTesting, TestEulerCreation) {
   // test default constructor
   Euler<float> default_euler;
-  EXPECT_FLOAT_EQ(0, default_euler.getX());
-  EXPECT_FLOAT_EQ(0, default_euler.getY());
-  EXPECT_FLOAT_EQ(0, default_euler.getZ());
+  EXPECT_FLOAT_EQ(0, default_euler.getX().getAngleValue());
+  EXPECT_FLOAT_EQ(0, default_euler.getY().getAngleValue());
+  EXPECT_FLOAT_EQ(0, default_euler.getZ().getAngleValue());
+  EXPECT_EQ(DEGREES, default_euler.getAngleType());
 
   // test parameterized constructor
-  Euler<float> param_euler(10, 15, 20);
-  EXPECT_FLOAT_EQ(10, param_euler.getX());
-  EXPECT_FLOAT_EQ(15, param_euler.getY());
-  EXPECT_FLOAT_EQ(20, param_euler.getZ());
+  Euler<float> param_euler(10, 15, 20, DEGREES);
+  EXPECT_FLOAT_EQ(10, param_euler.getX().getAngleValue());
+  EXPECT_FLOAT_EQ(15, param_euler.getY().getAngleValue());
+  EXPECT_FLOAT_EQ(20, param_euler.getZ().getAngleValue());
+  EXPECT_EQ(DEGREES, param_euler.getAngleType());
 
   // test copy constructor
   Euler<float> copied_euler(param_euler);
-  EXPECT_FLOAT_EQ(10, copied_euler.getX());
-  EXPECT_FLOAT_EQ(15, copied_euler.getY());
-  EXPECT_FLOAT_EQ(20, copied_euler.getZ());
+  EXPECT_FLOAT_EQ(10, copied_euler.getX().getAngleValue());
+  EXPECT_FLOAT_EQ(15, copied_euler.getY().getAngleValue());
+  EXPECT_FLOAT_EQ(20, copied_euler.getZ().getAngleValue());
+  EXPECT_EQ(DEGREES, copied_euler.getAngleType());
 
   // test copy assignment operator overload
   Euler<float> copied_assigned_euler;
   copied_assigned_euler = copied_euler;
-  EXPECT_FLOAT_EQ(10, copied_assigned_euler.getX());
-  EXPECT_FLOAT_EQ(15, copied_assigned_euler.getY());
-  EXPECT_FLOAT_EQ(20, copied_assigned_euler.getZ());
+  EXPECT_FLOAT_EQ(10, copied_assigned_euler.getX().getAngleValue());
+  EXPECT_FLOAT_EQ(15, copied_assigned_euler.getY().getAngleValue());
+  EXPECT_FLOAT_EQ(20, copied_assigned_euler.getZ().getAngleValue());
+  EXPECT_EQ(DEGREES, copied_assigned_euler.getAngleType());
 }
 
 TEST(EulerClassTesting, TestEulerSetGet) {
   // create zeroed out Euler
   Euler<float> new_euler;
-  EXPECT_FLOAT_EQ(0, new_euler.getX());
-  EXPECT_FLOAT_EQ(0, new_euler.getY());
-  EXPECT_FLOAT_EQ(0, new_euler.getZ());
+  EXPECT_FLOAT_EQ(0, new_euler.getX().getAngleValue());
+  EXPECT_FLOAT_EQ(0, new_euler.getY().getAngleValue());
+  EXPECT_FLOAT_EQ(0, new_euler.getZ().getAngleValue());
+  EXPECT_EQ(DEGREES, new_euler.getAngleType());
 
   // set values
-  new_euler.setX(15);
-  new_euler.setY(20);
-  new_euler.setZ(25);
+  new_euler.setX(Angle<float>(15, new_euler.getAngleType()));
+  new_euler.setY(Angle<float>(20, new_euler.getAngleType()));
+  new_euler.setZ(Angle<float>(25, new_euler.getAngleType()));
 
   // get values back and assert they are changed as expected
-  EXPECT_FLOAT_EQ(15, new_euler.getX());
-  EXPECT_FLOAT_EQ(20, new_euler.getY());
-  EXPECT_FLOAT_EQ(25, new_euler.getZ());
+  EXPECT_FLOAT_EQ(15, new_euler.getX().getAngleValue());
+  EXPECT_FLOAT_EQ(20, new_euler.getY().getAngleValue());
+  EXPECT_FLOAT_EQ(25, new_euler.getZ().getAngleValue());
+  EXPECT_EQ(DEGREES, new_euler.getAngleType());
+}
+
+TEST(EulerClassTesting, TestEulerAngleTypeConv) {
+  // create a test euler angle
+  Euler<float> new_euler(90, 270, 90, DEGREES);
+
+  // convert to radians
+  new_euler.toRadians();
+
+  // make sure all conversions worked
+  EXPECT_EQ(RADIANS, new_euler.getAngleType());
+  EXPECT_FLOAT_EQ(M_PI / 2, new_euler.getX().getAngleValue());
+  EXPECT_FLOAT_EQ((M_PI * 3) / 2, new_euler.getY().getAngleValue());
+  EXPECT_FLOAT_EQ(M_PI / 2, new_euler.getZ().getAngleValue());
+  EXPECT_EQ(RADIANS, new_euler.getX().getAngleType());
+  EXPECT_EQ(RADIANS, new_euler.getY().getAngleType());
+  EXPECT_EQ(RADIANS, new_euler.getZ().getAngleType());
+
+  // do the same thing in reverse
+  new_euler.toDegrees();
+
+  // verify we get our original values back
+  EXPECT_EQ(DEGREES, new_euler.getAngleType());
+  EXPECT_FLOAT_EQ(90, new_euler.getX().getAngleValue());
+  EXPECT_FLOAT_EQ(270, new_euler.getY().getAngleValue());
+  EXPECT_FLOAT_EQ(90, new_euler.getZ().getAngleValue());
+  EXPECT_EQ(DEGREES, new_euler.getX().getAngleType());
+  EXPECT_EQ(DEGREES, new_euler.getY().getAngleType());
+  EXPECT_EQ(DEGREES, new_euler.getZ().getAngleType());
+}
+
+TEST(EulerClassTesting, TestEulerToQuat) {
+  // create a few test euler angles
+  Euler<float> new_euler_one(45, 45, 45, DEGREES);
+  Euler<float> new_euler_two(M_PI / 2, M_PI / 2, M_PI / 2, RADIANS);
+
+  // generate quaternion and verify we get expected values
+  Quaternion<float> new_quat_one = new_euler_one.toQuaternion();
+  Quaternion<float> new_quat_two = new_euler_two.toQuaternion();
+
+  // make sure original Eulers were not mutated
+  EXPECT_EQ(DEGREES, new_euler_one.getAngleType());
+  EXPECT_EQ(RADIANS, new_euler_two.getAngleType());
+  EXPECT_FLOAT_EQ(45, new_euler_one.getX().getAngleValue());
+  EXPECT_FLOAT_EQ(45, new_euler_one.getY().getAngleValue());
+  EXPECT_FLOAT_EQ(45, new_euler_one.getZ().getAngleValue());
+  EXPECT_FLOAT_EQ(M_PI / 2, new_euler_two.getX().getAngleValue());
+  EXPECT_FLOAT_EQ(M_PI / 2, new_euler_two.getY().getAngleValue());
+  EXPECT_FLOAT_EQ(M_PI / 2, new_euler_two.getZ().getAngleValue());
+
+  // make sure quaternion values are correct
+  EXPECT_NEAR(0.8446, new_quat_one.getW(), 0.0001);
+  EXPECT_NEAR(0.1913, new_quat_one.getX(), 0.0001);
+  EXPECT_NEAR(0.4619, new_quat_one.getY(), 0.0001);
+  EXPECT_NEAR(0.1913, new_quat_one.getZ(), 0.0001);
+  EXPECT_NEAR(0.7071, new_quat_two.getW(), 0.0001);
+  EXPECT_NEAR(0.0, new_quat_two.getX(), 0.0001);
+  EXPECT_NEAR(0.7071, new_quat_two.getY(), 0.0001);
+  EXPECT_NEAR(0.0, new_quat_two.getZ(), 0.0001);
 }
 
 int main(int argc, char **argv) {
